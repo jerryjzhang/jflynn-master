a8e8d1bb464797b88498b804cd811894a5cb0bca
jerryjzhang <jerryjzhang@JERRYJZHANG-PC9.tencent.com>
2014-11-28 16:05:07 +0800

    update bootstrap.sh

--- flynn-controller/bin/bootstrap.sh
+++ flynn-controller/bin/bootstrap.sh
@@ -5,7 +5,7 @@ main_class="com.tencent.jflynn.boot.JFlynnMain"
 # get the path of this app
 app_path=`cd "$(dirname "$0")"; cd ..; pwd`
 
-run_cmd="nohup java -cp $app_path/classes -Djava.ext.dirs=$app_path/lib ${main_class}"
+run_cmd="java -cp $app_path/classes -Djava.ext.dirs=$app_path/lib ${main_class}"
 echo "start command: $run_cmd"
 echo "start..."
 
